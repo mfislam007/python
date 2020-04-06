1f1da7e811a0b8f1b1198e0d613de95eb40ab8c7
Md Farukul Islam <mfislam007@yahoo.com>
2020-04-06 22:24:38 +0300

    python 2 working file

---
+++ python2/3.1-Printing.py
@@ -0,0 +1,13 @@
+# Printing strings in Python
+print 'Hello World'
+print 'How are you doing today?'
+
+# Printing strings using variables
+a = 10 # Declaring variables 
+print 'There are', a, 'bikes available'
+b = 3
+c = a - b
+print 'The results of 10 - 3 is ', c
+
+
+
